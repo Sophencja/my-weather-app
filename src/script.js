a507057774059b983dd5df265f60d1f00c27b95a
Sophencja <miszkielasia7@gmail.com>
2021-03-29 19:31:39 +0200

    updated style.css, script.js, index.html

--- src/script.js
+++ src/script.js
@@ -112,7 +112,7 @@ function displayForecast(response) {
   forecastElement.innerHTML = null;
   let forecast = null;
 
-  for (let index = 0; index < 6; index++) {
+  for (let index = 0; index < 8; index++) {
     forecast = response.data.list[index];
     forecastElement.innerHTML += `
      <div class="col forecast">
